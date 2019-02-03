9480de99cb8e6e49cc6b8b5b70869ef433edd0a5
daira4000 <daira4000@gmail.com>
2019-02-03 21:01:50 +0900

    [AC] abc117 abc117_a

---
+++ atcoder.jp/abc117/abc117_a/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+	static Scanner in = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		int T = in.nextInt();
+		int X = in.nextInt();
+		System.out.println((float) T / X);
+	}
+}
