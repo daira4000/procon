905da797b205130827a4c2dbf68ccdaee88440de
daira4000 <daira4000@gmail.com>
2019-04-04 00:22:12 +0900

    [AC] exawizards2019 exawizards2019_a

---
+++ atcoder.jp/exawizards2019/exawizards2019_a/Main.java
@@ -0,0 +1,36 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        Scanner in = new Scanner(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        exawizards2019_a solver = new exawizards2019_a();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class exawizards2019_a {
+        public void solve(int testNumber, Scanner in, PrintWriter out) {
+            int A = in.nextInt();
+            int B = in.nextInt();
+            int C = in.nextInt();
+            if (A == B && B == C) {
+                out.println("Yes");
+            } else {
+                out.println("No");
+            }
+        }
+
+    }
+}
+
