2a5a5aee69050d416a9d06b4a067230df025ca71
daira4000 <daira4000@gmail.com>
2019-04-20 21:10:47 +0900

    [AC] tenka1-2019-beginner tenka1_2019_b

---
+++ atcoder.jp/tenka1-2019-beginner/tenka1_2019_b/Main.java
@@ -0,0 +1,39 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        Scanner in = new Scanner(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        B solver = new B();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class B {
+        public void solve(int testNumber, Scanner in, PrintWriter out) {
+            int N = in.nextInt();
+            String S = in.next();
+            int K = in.nextInt();
+            char c = S.charAt(K - 1);
+            char[] ss = S.toCharArray();
+            for (int i = 0; i < ss.length; i++) {
+                if (ss[i] != c) {
+                    ss[i] = '*';
+                }
+            }
+            out.println(new String(ss));
+        }
+
+    }
+}
+
