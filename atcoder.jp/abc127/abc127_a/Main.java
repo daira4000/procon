764eed35e62176b9ff9638d53bc3e3bd432e726a
daira4000 <daira4000@gmail.com>
2019-05-26 17:14:09 +0900

    [AC] abc127 abc127_a

---
+++ atcoder.jp/abc127/abc127_a/Main.java
@@ -0,0 +1,37 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        Scanner in = new Scanner(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        A solver = new A();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class A {
+        public void solve(int testNumber, Scanner in, PrintWriter out) {
+            int A = in.nextInt();
+            int B = in.nextInt();
+            if (13 <= A) {
+                out.println(B);
+            } else if (6 <= A) {
+                out.println(B / 2);
+            } else {
+                out.println(0);
+            }
+        }
+
+    }
+}
+
