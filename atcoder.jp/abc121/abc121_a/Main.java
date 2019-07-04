a3e750e33a209c61ef7b2d37d6d1595a6493627a
daira4000 <daira4000@gmail.com>
2019-07-05 00:38:46 +0900

    [AC] abc121 abc121_a

---
+++ atcoder.jp/abc121/abc121_a/Main.java
@@ -0,0 +1,33 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        Scanner in = new Scanner(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        A solver = new A();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class A {
+        public void solve(int testNumber, Scanner in, PrintWriter out) {
+            int H = in.nextInt();
+            int W = in.nextInt();
+            int h = in.nextInt();
+            int w = in.nextInt();
+            out.println(H * W - (H * w) - (W * h) + (h * w));
+        }
+
+    }
+}
+
