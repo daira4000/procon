4e4146aa5c4e193430e5aa7c0649e14b1349eea1
daira4000 <daira4000@gmail.com>
2019-07-07 21:01:21 +0900

    [AC] abc133 abc133_a

---
+++ atcoder.jp/abc133/abc133_a/Main.java
@@ -0,0 +1,32 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        Scanner in = new Scanner(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        A solver = new A();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class A {
+        public void solve(int testNumber, Scanner in, PrintWriter out) {
+            int N = in.nextInt();
+            int A = in.nextInt();
+            int B = in.nextInt();
+            out.println(Math.min(A * N, B));
+        }
+
+    }
+}
+
