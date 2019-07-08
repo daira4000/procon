75cb1274738ac486f79f1b4015bd9a726d93447c
daira4000 <daira4000@gmail.com>
2019-07-08 21:20:19 +0900

    [AC] abc115 abc115_b

---
+++ atcoder.jp/abc115/abc115_b/Main.java
@@ -0,0 +1,39 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.stream.IntStream;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.OptionalInt;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        Scanner in = new Scanner(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        B solver = new B();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class B {
+        public void solve(int testNumber, Scanner in, PrintWriter out) {
+            int N = in.nextInt();
+            int[] p = new int[N];
+            for (int i = 0; i < N; i++) {
+                p[i] = in.nextInt();
+            }
+            int max = Arrays.stream(p).max().getAsInt();
+            int sum = Arrays.stream(p).sum();
+            out.println(sum - max / 2);
+        }
+
+    }
+}
+
