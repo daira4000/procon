952e1718f4c34db32c84ed0d4f6b238cbba93547
daira4000 <daira4000@gmail.com>
2019-07-20 21:03:31 +0900

    [AC] abc134 abc134_b

---
+++ atcoder.jp/abc134/abc134_b/Main.java
@@ -0,0 +1,31 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        Scanner in = new Scanner(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        B solver = new B();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class B {
+        public void solve(int testNumber, Scanner in, PrintWriter out) {
+            int N = in.nextInt();
+            int D = in.nextInt();
+            out.println((int) Math.ceil(N / (1.0 + 2 * D)));
+        }
+
+    }
+}
+
