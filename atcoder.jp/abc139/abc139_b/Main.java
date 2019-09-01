cd71e4450d8d603e8893561500ec7a0439d465a7
daira4000 <daira4000@gmail.com>
2019-09-01 21:04:39 +0900

    [AC] abc139 abc139_b

---
+++ atcoder.jp/abc139/abc139_b/Main.java
@@ -0,0 +1,37 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        Scanner in = new Scanner(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        B solver = new B();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class B {
+        public void solve(int testNumber, Scanner in, PrintWriter out) {
+            int A = in.nextInt();
+            int B = in.nextInt();
+            int cnt = 0;
+            int cur = 1;
+            while (cur < B) {
+                cur = cur - 1 + A;
+                cnt++;
+            }
+            out.println(cnt);
+        }
+
+    }
+}
+
