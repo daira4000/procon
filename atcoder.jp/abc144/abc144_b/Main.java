42addd95fd038df130f08400b9edc7e523ca53b2
daira4000 <daira4000@gmail.com>
2019-10-27 21:04:31 +0900

    [AC] abc144 abc144_b

---
+++ atcoder.jp/abc144/abc144_b/Main.java
@@ -0,0 +1,36 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        Scanner in = new Scanner(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        B solver = new B();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class B {
+        public void solve(int testNumber, Scanner in, PrintWriter out) {
+            int N = in.nextInt();
+            for (int i = 1; i <= Math.min(9, N); i++) {
+                if (N % i == 0 && N / i < 10) {
+                    out.println("Yes");
+                    return;
+                }
+            }
+            out.println("No");
+        }
+
+    }
+}
+
