21c41da67d6dfd450464aea882107a6b1a286738
daira4000 <daira4000@gmail.com>
2019-11-09 01:59:14 +0900

    [AC] abc137 abc137_c

---
+++ atcoder.jp/abc137/abc137_c/Main.java
@@ -0,0 +1,87 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.io.BufferedReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        MyScanner in = new MyScanner(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        C solver = new C();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class C {
+        public void solve(int testNumber, MyScanner in, PrintWriter out) {
+            int N = in.nextInt();
+            Map<String, Integer> map = new HashMap<>();
+            for (int i = 0; i < N; i++) {
+                String s = in.next();
+                char[] cs = s.toCharArray();
+                Arrays.sort(cs);
+                s = new String(cs);
+                map.put(s, map.getOrDefault(s, 0) + 1);
+            }
+
+            long sum = 0;
+            for (Map.Entry<String, Integer> entry : map.entrySet()) {
+                long value = entry.getValue();
+                if (value > 1) {
+                    long temp = 0;
+                    for (long i = 2; i <= value; i++) {
+                        temp += i - 1;
+                    }
+                    sum += temp;
+                }
+            }
+            out.println(sum);
+        }
+
+    }
+
+    static class MyScanner {
+        private BufferedReader in;
+        private StringTokenizer st;
+
+        public MyScanner(InputStream stream) {
+            in = new BufferedReader(new InputStreamReader(stream));
+        }
+
+        public String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    String rl = in.readLine();
+                    if (rl == null) {
+                        return null;
+                    }
+                    st = new StringTokenizer(rl);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+    }
+}
+
