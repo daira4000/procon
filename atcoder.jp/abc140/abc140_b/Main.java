8eb3fb4039230b5415f3eba6304f7794ca628c4f
daira4000 <daira4000@gmail.com>
2019-11-09 01:14:00 +0900

    [AC] abc140 abc140_b

---
+++ atcoder.jp/abc140/abc140_b/Main.java
@@ -0,0 +1,85 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        MyScanner in = new MyScanner(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        B solver = new B();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class B {
+        public void solve(int testNumber, MyScanner in, PrintWriter out) {
+            int N = in.nextInt();
+            int[] A = in.nextIntArray(N);
+            int[] B = in.nextIntArray(N);
+            int[] C = in.nextIntArray(N - 1);
+
+            long sum = 0;
+            int prev = -2;
+            for (int i = 0; i < N; i++) {
+                int n = A[i] - 1;
+                sum += B[n];
+                if (prev + 1 == n) {
+                    sum += C[prev];
+                }
+                prev = n;
+            }
+            out.println(sum);
+        }
+
+    }
+
+    static class MyScanner {
+        private BufferedReader in;
+        private StringTokenizer st;
+
+        public MyScanner(InputStream stream) {
+            in = new BufferedReader(new InputStreamReader(stream));
+        }
+
+        public String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    String rl = in.readLine();
+                    if (rl == null) {
+                        return null;
+                    }
+                    st = new StringTokenizer(rl);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public int[] nextIntArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = nextInt();
+            }
+            return a;
+        }
+
+    }
+}
+
