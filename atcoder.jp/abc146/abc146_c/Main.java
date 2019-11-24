2317a6913598d216417e26c58ba86c492e29edc5
daira4000 <daira4000@gmail.com>
2019-11-24 21:20:28 +0900

    [AC] abc146 abc146_c

---
+++ atcoder.jp/abc146/abc146_c/Main.java
@@ -0,0 +1,81 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        MyScanner in = new MyScanner(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        C solver = new C();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class C {
+        public void solve(int testNumber, MyScanner in, PrintWriter out) {
+            long A = in.nextLong();
+            long B = in.nextLong();
+            long X = in.nextLong();
+
+            long l = 0;
+            long r = X / A + 1;
+            while (l < r - 1) {
+                long m = (r + l) / 2;
+                long t = calc(A, B, m);
+                if (t <= X) {
+                    l = m;
+                } else {
+                    r = m;
+                }
+            }
+            out.println(Math.min(l, (long) 1e9));
+        }
+
+        private long calc(long a, long b, long m) {
+            return a * m + b * (String.valueOf(m).length());
+        }
+
+    }
+
+    static class MyScanner {
+        private BufferedReader in;
+        private StringTokenizer st;
+
+        public MyScanner(InputStream stream) {
+            in = new BufferedReader(new InputStreamReader(stream));
+        }
+
+        public String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    String rl = in.readLine();
+                    if (rl == null) {
+                        return null;
+                    }
+                    st = new StringTokenizer(rl);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return st.nextToken();
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+    }
+}
+
