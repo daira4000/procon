5c7ec9989b2baa50434f0604fdcfa8dddef59d31
daira4000 <daira4000@gmail.com>
2019-12-09 22:04:50 +0900

    [AC] abc147 abc147_b

---
+++ atcoder.jp/abc147/abc147_b/Main.java
@@ -0,0 +1,65 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        MyScanner in = new MyScanner(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        B solver = new B();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class B {
+        public void solve(int testNumber, MyScanner in, PrintWriter out) {
+            String S = in.next();
+            int count = 0;
+            for (int i = 0; i < S.length() / 2; i++) {
+                if (S.charAt(i) != S.charAt(S.length() - 1 - i)) {
+                    count++;
+                }
+            }
+            out.println(count);
+        }
+
+    }
+
+    static class MyScanner {
+        private BufferedReader in;
+        private StringTokenizer st;
+
+        public MyScanner(InputStream stream) {
+            in = new BufferedReader(new InputStreamReader(stream));
+        }
+
+        public String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    String rl = in.readLine();
+                    if (rl == null) {
+                        return null;
+                    }
+                    st = new StringTokenizer(rl);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return st.nextToken();
+        }
+
+    }
+}
+
