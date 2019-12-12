f21348eb8d1d741f66eb0ec7a22d2da6510d72b4
daira4000 <daira4000@gmail.com>
2019-12-12 23:19:46 +0900

    [AC] abc147 abc147_d

---
+++ atcoder.jp/abc147/abc147_d/Main.java
@@ -0,0 +1,93 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        MyScanner in = new MyScanner(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        D solver = new D();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class D {
+        static long MOD = (long) 1e9 + 7;
+
+        public void solve(int testNumber, MyScanner in, PrintWriter out) {
+            int N = in.nextInt();
+            long[] A = in.nextLongArray(N);
+
+            long ans = 0;
+            for (int i = 0; i < 61; i++) {
+                long d = 1L << i;
+                long count = 0;
+                for (int j = 0; j < N; j++) {
+                    if ((A[j] & d) > 0) {
+                        count++;
+                    }
+                }
+                long temp = (count * (N - count)) % MOD;
+                ans += (d % MOD) * temp % MOD;
+                ans %= MOD;
+            }
+
+            out.println(ans);
+        }
+
+    }
+
+    static class MyScanner {
+        private BufferedReader in;
+        private StringTokenizer st;
+
+        public MyScanner(InputStream stream) {
+            in = new BufferedReader(new InputStreamReader(stream));
+        }
+
+        public String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    String rl = in.readLine();
+                    if (rl == null) {
+                        return null;
+                    }
+                    st = new StringTokenizer(rl);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        public long[] nextLongArray(int n) {
+            long[] a = new long[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = nextLong();
+            }
+            return a;
+        }
+
+    }
+}
+
