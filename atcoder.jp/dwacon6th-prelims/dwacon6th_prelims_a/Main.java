c83ff34895360010309429d59661b2d33b87a299
daira4000 <daira4000@gmail.com>
2020-01-11 20:03:28 +0900

    [AC] dwacon6th-prelims dwacon6th_prelims_a

---
+++ atcoder.jp/dwacon6th-prelims/dwacon6th_prelims_a/Main.java
@@ -0,0 +1,80 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        MyScanner in = new MyScanner(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        A solver = new A();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class A {
+        public void solve(int testNumber, MyScanner in, PrintWriter out) {
+            int N = in.nextInt();
+            String[] s = new String[N];
+            int[] t = new int[N];
+            for (int i = 0; i < N; i++) {
+                s[i] = in.next();
+                t[i] = in.nextInt();
+            }
+            String X = in.next();
+
+            boolean f = false;
+            int sum = 0;
+            for (int i = 0; i < N; i++) {
+                if (f) {
+                    sum += t[i];
+                } else if (X.equals(s[i])) {
+                    f = true;
+                }
+            }
+            out.println(sum);
+        }
+
+    }
+
+    static class MyScanner {
+        private BufferedReader in;
+        private StringTokenizer st;
+
+        public MyScanner(InputStream stream) {
+            in = new BufferedReader(new InputStreamReader(stream));
+        }
+
+        public String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    String rl = in.readLine();
+                    if (rl == null) {
+                        return null;
+                    }
+                    st = new StringTokenizer(rl);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+    }
+}
+
