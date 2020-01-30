68e7f1c25ef5cd5f4410e855f36f165e488548ef
daira4000 <daira4000@gmail.com>
2020-01-30 22:55:08 +0900

    [AC] sumitrust2019 sumitb2019_e

---
+++ atcoder.jp/sumitrust2019/sumitb2019_e/Main.java
@@ -0,0 +1,88 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        MyScanner in = new MyScanner(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        E solver = new E();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class E {
+        static long MOD = 1000000007L;
+
+        public void solve(int testNumber, MyScanner in, PrintWriter out) {
+            int N = in.nextInt();
+            int[] A = in.nextIntArray(N);
+
+            int[] cnt = new int[N];
+            Arrays.fill(cnt, 3);
+            long ans = 1;
+            for (int i = 0; i < N; i++) {
+                if (A[i] == 0) {
+                    ans *= cnt[A[i]]--;
+                } else {
+                    ans *= (cnt[A[i]] - cnt[A[i] - 1]);
+                    cnt[A[i]]--;
+                }
+                ans %= MOD;
+            }
+            out.println(ans);
+        }
+
+    }
+
+    static class MyScanner {
+        private BufferedReader in;
+        private StringTokenizer st;
+
+        public MyScanner(InputStream stream) {
+            in = new BufferedReader(new InputStreamReader(stream));
+        }
+
+        public String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    String rl = in.readLine();
+                    if (rl == null) {
+                        return null;
+                    }
+                    st = new StringTokenizer(rl);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public int[] nextIntArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = nextInt();
+            }
+            return a;
+        }
+
+    }
+}
+
