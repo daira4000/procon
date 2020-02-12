c962ee81b45b3acee1cf2220d38c44ef7bb0cfde
daira4000 <daira4000@gmail.com>
2020-02-12 23:45:27 +0900

    [AC] abc111 abc111_a

---
+++ atcoder.jp/abc111/abc111_a/Main.java
@@ -0,0 +1,66 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        MyScanner in = new MyScanner(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        A solver = new A();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class A {
+        public void solve(int testNumber, MyScanner in, PrintWriter out) {
+            char[] n = in.next().toCharArray();
+            for (int i = 0; i < 3; i++) {
+                if (n[i] == '1') {
+                    n[i] = '9';
+                } else if (n[i] == '9') {
+                    n[i] = '1';
+                }
+            }
+            out.println(Integer.parseInt(new String(n)));
+        }
+
+    }
+
+    static class MyScanner {
+        private BufferedReader in;
+        private StringTokenizer st;
+
+        public MyScanner(InputStream stream) {
+            in = new BufferedReader(new InputStreamReader(stream));
+        }
+
+        public String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    String rl = in.readLine();
+                    if (rl == null) {
+                        return null;
+                    }
+                    st = new StringTokenizer(rl);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return st.nextToken();
+        }
+
+    }
+}
+
