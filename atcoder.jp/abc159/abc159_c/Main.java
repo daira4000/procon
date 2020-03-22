3b403dc2337b353d207913671d04d9ca15b0ff63
daira4000 <daira4000@gmail.com>
2020-03-22 21:24:50 +0900

    [AC] abc159 abc159_c

---
+++ atcoder.jp/abc159/abc159_c/Main.java
@@ -0,0 +1,64 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        MyScanner in = new MyScanner(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        C solver = new C();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class C {
+        public void solve(int testNumber, MyScanner in, PrintWriter out) {
+            int L = in.nextInt();
+            double b = L / 3.0;
+            out.printf("%.12f\n", Math.pow(b, 3));
+        }
+
+    }
+
+    static class MyScanner {
+        private BufferedReader in;
+        private StringTokenizer st;
+
+        public MyScanner(InputStream stream) {
+            in = new BufferedReader(new InputStreamReader(stream));
+        }
+
+        public String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    String rl = in.readLine();
+                    if (rl == null) {
+                        return null;
+                    }
+                    st = new StringTokenizer(rl);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+    }
+}
+
