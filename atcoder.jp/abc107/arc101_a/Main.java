0ed8e2c1d29369c3cf2dc3811b9678a8558378be
daira4000 <daira4000@gmail.com>
2020-03-30 23:46:58 +0900

    [AC] abc107 arc101_a

---
+++ atcoder.jp/abc107/arc101_a/Main.java
@@ -0,0 +1,85 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.stream.IntStream;
+import java.util.StringTokenizer;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        MyScanner in = new MyScanner(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        C solver = new C();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class C {
+        public void solve(int testNumber, MyScanner in, PrintWriter out) {
+            int N = in.nextInt();
+            int K = in.nextInt();
+            int[] x = in.nextIntArray(N);
+            int nega = (int) IntStream.of(x).filter(n -> n <= 0).count();
+            int min = 1010101010;
+            for (int i = 0; i + K <= N; i++) {
+                if (i + K <= nega) {
+                    min = Math.min(min, -x[i]);
+                } else if (nega < i + 1) {
+                    min = Math.min(min, x[i + K - 1]);
+                } else {
+                    min = Math.min(min, Math.min(-x[i] * 2 + x[i + K - 1], -x[i] + x[i + K - 1] * 2));
+                }
+            }
+            out.println(min);
+        }
+
+    }
+
+    static class MyScanner {
+        private BufferedReader in;
+        private StringTokenizer st;
+
+        public MyScanner(InputStream stream) {
+            in = new BufferedReader(new InputStreamReader(stream));
+        }
+
+        public String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    String rl = in.readLine();
+                    if (rl == null) {
+                        return null;
+                    }
+                    st = new StringTokenizer(rl);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public int[] nextIntArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = nextInt();
+            }
+            return a;
+        }
+
+    }
+}
+
