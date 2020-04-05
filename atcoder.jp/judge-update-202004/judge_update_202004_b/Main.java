dba4de65788b5e5723750ab68e4c2b40681935cf
daira4000 <daira4000@gmail.com>
2020-04-05 21:04:50 +0900

    [AC] judge-update-202004 judge_update_202004_b

---
+++ atcoder.jp/judge-update-202004/judge_update_202004_b/Main.java
@@ -0,0 +1,76 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.util.Comparator;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        MyScanner in = new MyScanner(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        B solver = new B();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class B {
+        public void solve(int testNumber, MyScanner in, PrintWriter out) {
+            int N = in.nextInt();
+            ArrayList<Integer> red = new ArrayList<>();
+            ArrayList<Integer> blue = new ArrayList<>();
+            for (int i = 0; i < N; i++) {
+                int X = in.nextInt();
+                char C = in.next().toCharArray()[0];
+                if (C == 'R') red.add(X);
+                else blue.add(X);
+            }
+            red.sort(Comparator.naturalOrder());
+            blue.sort(Comparator.naturalOrder());
+            red.forEach(out::println);
+            blue.forEach(out::println);
+        }
+
+    }
+
+    static class MyScanner {
+        private BufferedReader in;
+        private StringTokenizer st;
+
+        public MyScanner(InputStream stream) {
+            in = new BufferedReader(new InputStreamReader(stream));
+        }
+
+        public String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    String rl = in.readLine();
+                    if (rl == null) {
+                        return null;
+                    }
+                    st = new StringTokenizer(rl);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+    }
+}
+
