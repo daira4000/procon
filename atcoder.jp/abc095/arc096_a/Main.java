2a63628752c1111b40c89e5490317cf103be149e
daira4000 <daira4000@gmail.com>
2020-04-16 13:28:44 +0900

    [AC] abc095 arc096_a

---
+++ atcoder.jp/abc095/arc096_a/Main.java
@@ -0,0 +1,75 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        MyScanner in = new MyScanner(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        C solver = new C();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class C {
+        public void solve(int testNumber, MyScanner in, PrintWriter out) {
+            int A = in.Int();
+            int B = in.Int();
+            int C = in.Int();
+            int X = in.Int();
+            int Y = in.Int();
+
+            int Z = Math.max(X, Y) * 2;
+            int min = (int) 1e10;
+            for (int i = 0; i <= Z; i += 2) {
+                int n = i / 2;
+                int cost = A * Math.max(0, X - n) + B * Math.max(0, Y - n) + C * i;
+                min = Math.min(min, cost);
+            }
+            out.println(min);
+        }
+
+    }
+
+    static class MyScanner {
+        private BufferedReader in;
+        private StringTokenizer st;
+
+        public MyScanner(InputStream stream) {
+            in = new BufferedReader(new InputStreamReader(stream));
+        }
+
+        public String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    String rl = in.readLine();
+                    if (rl == null) {
+                        return null;
+                    }
+                    st = new StringTokenizer(rl);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int Int() {
+            return Integer.parseInt(next());
+        }
+
+    }
+}
+
