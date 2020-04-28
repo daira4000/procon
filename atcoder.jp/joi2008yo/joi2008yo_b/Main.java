7debe050578ea182aee7751f2cf6f99fb0f0e3ad
daira4000 <daira4000@gmail.com>
2020-04-28 23:30:01 +0900

    [AC] joi2008yo joi2008yo_b

---
+++ atcoder.jp/joi2008yo/joi2008yo_b/Main.java
@@ -0,0 +1,72 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        MyScanner in = new MyScanner(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        B solver = new B();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class B {
+        public void solve(int testNumber, MyScanner in, PrintWriter out) {
+            String S = in.next();
+            String joi = "JOI";
+            String ioi = "IOI";
+            int cntj = 0;
+            int cnti = 0;
+            for (int i = 0; i < S.length() - 2; i++) {
+                String tmp = S.substring(i, i + 3);
+                if (tmp.equals(joi)) {
+                    cntj++;
+                } else if (tmp.equals(ioi)) {
+                    cnti++;
+                }
+            }
+            out.println(cntj);
+            out.println(cnti);
+        }
+
+    }
+
+    static class MyScanner {
+        private BufferedReader in;
+        private StringTokenizer st;
+
+        public MyScanner(InputStream stream) {
+            in = new BufferedReader(new InputStreamReader(stream));
+        }
+
+        public String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    String rl = in.readLine();
+                    if (rl == null) {
+                        return null;
+                    }
+                    st = new StringTokenizer(rl);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return st.nextToken();
+        }
+
+    }
+}
+
