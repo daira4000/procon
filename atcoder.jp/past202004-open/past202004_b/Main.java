a5f166883af398b7710ca798afd6682ed86e6ef2
daira4000 <daira4000@gmail.com>
2020-05-05 16:36:31 +0900

    [AC] past202004-open past202004_b

---
+++ atcoder.jp/past202004-open/past202004_b/Main.java
@@ -0,0 +1,68 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.stream.IntStream;
+import java.util.StringTokenizer;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        MyScanner in = new MyScanner(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        B solver = new B();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class B {
+        public void solve(int testNumber, MyScanner in, PrintWriter out) {
+            String S = in.next();
+            int[] cnt = new int[3];
+            S.chars().forEach(c -> cnt[c - 'a']++);
+            if (cnt[0] > cnt[1] && cnt[0] > cnt[2]) {
+                out.println("a");
+            } else if (cnt[1] > cnt[2]) {
+                out.println("b");
+            } else {
+                out.println("c");
+            }
+        }
+
+    }
+
+    static class MyScanner {
+        private BufferedReader in;
+        private StringTokenizer st;
+
+        public MyScanner(InputStream stream) {
+            in = new BufferedReader(new InputStreamReader(stream));
+        }
+
+        public String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    String rl = in.readLine();
+                    if (rl == null) {
+                        return null;
+                    }
+                    st = new StringTokenizer(rl);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return st.nextToken();
+        }
+
+    }
+}
+
