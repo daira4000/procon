8ee0eaf0eeeb6d8988ba34c894bbc46d2b19c18b
daira4000 <daira4000@gmail.com>
2020-05-12 23:28:41 +0900

    [AC] agc032 agc032_a

---
+++ atcoder.jp/agc032/agc032_a/Main.java
@@ -0,0 +1,96 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.stream.IntStream;
+import java.io.IOException;
+import java.util.stream.Collectors;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.util.Collections;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        MyScanner in = new MyScanner(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        A solver = new A();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class A {
+        public void solve(int testNumber, MyScanner in, PrintWriter out) {
+            int N = in.Int();
+            int[] b = in.Int(N);
+            List<Integer> ans = new ArrayList<>();
+            List<Integer> tmp = IntStream.of(b).boxed().collect(Collectors.toCollection(ArrayList::new));
+            for (int i = 0; i < N; i++) {
+                boolean f = false;
+                for (int j = N - i; j > 0; j--) {
+                    if (tmp.get(j - 1) == j) {
+                        tmp.remove(j - 1);
+                        ans.add(j);
+                        f = true;
+                        break;
+                    }
+                }
+                if (!f) {
+                    out.println(-1);
+                    return;
+                }
+            }
+            Collections.reverse(ans);
+            ans.forEach(out::println);
+        }
+
+    }
+
+    static class MyScanner {
+        private BufferedReader in;
+        private StringTokenizer st;
+
+        public MyScanner(InputStream stream) {
+            in = new BufferedReader(new InputStreamReader(stream));
+        }
+
+        public String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    String rl = in.readLine();
+                    if (rl == null) {
+                        return null;
+                    }
+                    st = new StringTokenizer(rl);
+                } catch (IOException e) {
+                    return null;
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int Int() {
+            return Integer.parseInt(next());
+        }
+
+        public int[] Int(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = Int();
+            }
+            return a;
+        }
+
+    }
+}
+
