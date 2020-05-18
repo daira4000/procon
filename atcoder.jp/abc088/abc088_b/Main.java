bbc6cc9a06ff3ebff3b3f6762ed30abfe8356215
daira4000 <daira4000@gmail.com>
2020-05-18 20:28:03 +0900

    [AC] abc088 abc088_b

---
+++ atcoder.jp/abc088/abc088_b/Main.java
@@ -0,0 +1,84 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        MyScanner in = new MyScanner(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        B solver = new B();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class B {
+        public void solve(int testNumber, MyScanner in, PrintWriter out) {
+            int N = in.Int();
+            int[] a = in.Int(N);
+            int x = 0;
+            int y = 0;
+            Arrays.sort(a);
+            for (int i = 0; i < N; i++) {
+                int v = a[N - 1 - i];
+                if (i % 2 == 0) {
+                    x += v;
+                } else {
+                    y += v;
+                }
+            }
+            out.println(x - y);
+        }
+
+    }
+
+    static class MyScanner {
+        private BufferedReader in;
+        private StringTokenizer st;
+
+        public MyScanner(InputStream stream) {
+            in = new BufferedReader(new InputStreamReader(stream));
+        }
+
+        public String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    String rl = in.readLine();
+                    if (rl == null) {
+                        return null;
+                    }
+                    st = new StringTokenizer(rl);
+                } catch (IOException e) {
+                    return null;
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int Int() {
+            return Integer.parseInt(next());
+        }
+
+        public int[] Int(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = Int();
+            }
+            return a;
+        }
+
+    }
+}
+
