2b350a4c5bde2a99697e286a1040a18d23b8d35a
daira4000 <daira4000@gmail.com>
2020-05-18 18:59:43 +0900

    [AC] abc061 abc061_c

---
+++ atcoder.jp/abc061/abc061_c/Main.java
@@ -0,0 +1,84 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.TreeMap;
+import java.util.StringTokenizer;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        MyScanner in = new MyScanner(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        C solver = new C();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class C {
+        public void solve(int testNumber, MyScanner in, PrintWriter out) {
+            int N = in.Int();
+            long K = in.Long();
+            Map<Integer, Long> map = new TreeMap<>();
+            for (int i = 0; i < N; i++) {
+                int a = in.Int();
+                int b = in.Int();
+                map.put(a, map.getOrDefault(a, 0L) + b);
+            }
+            long cnt = 0;
+            for (Map.Entry<Integer, Long> e : map.entrySet()) {
+                cnt += e.getValue();
+                if (cnt >= K) {
+                    out.println(e.getKey());
+                    return;
+                }
+            }
+        }
+
+    }
+
+    static class MyScanner {
+        private BufferedReader in;
+        private StringTokenizer st;
+
+        public MyScanner(InputStream stream) {
+            in = new BufferedReader(new InputStreamReader(stream));
+        }
+
+        public String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    String rl = in.readLine();
+                    if (rl == null) {
+                        return null;
+                    }
+                    st = new StringTokenizer(rl);
+                } catch (IOException e) {
+                    return null;
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int Int() {
+            return Integer.parseInt(next());
+        }
+
+        public long Long() {
+            return Long.parseLong(next());
+        }
+
+    }
+}
+
