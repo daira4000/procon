9ff62fa5865d160ba9bec659cd5bfbb6861d6bfd
daira4000 <daira4000@gmail.com>
2020-06-21 21:10:24 +0900

    [AC] abc171 abc171_c

---
+++ atcoder.jp/abc171/abc171_c/Main.java
@@ -0,0 +1,73 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.util.Collections;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        MyScanner in = new MyScanner(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        C solver = new C();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class C {
+        public void solve(int testNumber, MyScanner in, PrintWriter out) {
+            long N = in.Long();
+            List<String> name = new ArrayList<>();
+            while (N > 0) {
+                int a = (int) ((N - 1) % 26);
+                N = (N - 1) / 26;
+                name.add(String.valueOf((char) ('a' + a)));
+            }
+            Collections.reverse(name);
+            out.println(String.join("", name));
+        }
+
+    }
+
+    static class MyScanner {
+        private BufferedReader in;
+        private StringTokenizer st;
+
+        public MyScanner(InputStream stream) {
+            in = new BufferedReader(new InputStreamReader(stream));
+        }
+
+        public String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    String rl = in.readLine();
+                    if (rl == null) {
+                        return null;
+                    }
+                    st = new StringTokenizer(rl);
+                } catch (IOException e) {
+                    return null;
+                }
+            }
+            return st.nextToken();
+        }
+
+        public long Long() {
+            return Long.parseLong(next());
+        }
+
+    }
+}
+
