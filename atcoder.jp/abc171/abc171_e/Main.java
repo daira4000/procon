6ffd5f2bc7d7a1760ce7837dbeae24b368b99ffe
daira4000 <daira4000@gmail.com>
2020-06-21 21:44:14 +0900

    [AC] abc171 abc171_e

---
+++ atcoder.jp/abc171/abc171_e/Main.java
@@ -0,0 +1,85 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+import java.util.StringTokenizer;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.util.stream.Collectors;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        MyScanner in = new MyScanner(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        E solver = new E();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class E {
+        public void solve(int testNumber, MyScanner in, PrintWriter out) {
+            int N = in.Int();
+            int[] a = in.Int(N);
+
+            int[] ans = new int[N];
+            int all = 0;
+            for (int i = 0; i < N; i++) {
+                all ^= a[i];
+            }
+            for (int i = 0; i < N; i++) {
+                ans[i] = a[i] ^ all;
+            }
+
+            out.println(IntStream.of(ans).mapToObj(Integer::toString).collect(Collectors.joining(" ")));
+        }
+
+    }
+
+    static class MyScanner {
+        private BufferedReader in;
+        private StringTokenizer st;
+
+        public MyScanner(InputStream stream) {
+            in = new BufferedReader(new InputStreamReader(stream));
+        }
+
+        public String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    String rl = in.readLine();
+                    if (rl == null) {
+                        return null;
+                    }
+                    st = new StringTokenizer(rl);
+                } catch (IOException e) {
+                    return null;
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int Int() {
+            return Integer.parseInt(next());
+        }
+
+        public int[] Int(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = Int();
+            }
+            return a;
+        }
+
+    }
+}
+
