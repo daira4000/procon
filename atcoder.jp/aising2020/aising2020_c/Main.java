d4fde732ba008c940ece5c77b42d9b4385816d34
daira4000 <daira4000@gmail.com>
2020-07-11 21:11:45 +0900

    [AC] aising2020 aising2020_c

---
+++ atcoder.jp/aising2020/aising2020_c/Main.java
@@ -0,0 +1,78 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+import java.util.Map;
+import java.util.HashMap;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        MyScanner in = new MyScanner(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        C solver = new C();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class C {
+        public void solve(int testNumber, MyScanner in, PrintWriter out) {
+            int N = in.Int();
+
+            Map<Integer, Integer> map = new HashMap<>();
+            for (int x = 1; x <= 1000; x++) {
+                for (int y = 1; y <= 1000; y++) {
+                    for (int z = 1; z <= 1000; z++) {
+                        int key = x * x + y * y + z * z + x * y + y * z + z * x;
+                        if (key > N) break;
+                        map.merge(key, 1, Integer::sum);
+                    }
+                }
+            }
+            for (int i = 1; i <= N; i++) {
+                out.println(map.getOrDefault(i, 0));
+            }
+        }
+
+    }
+
+    static class MyScanner {
+        private BufferedReader in;
+        private StringTokenizer st;
+
+        public MyScanner(InputStream stream) {
+            in = new BufferedReader(new InputStreamReader(stream));
+        }
+
+        public String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    String rl = in.readLine();
+                    if (rl == null) {
+                        return null;
+                    }
+                    st = new StringTokenizer(rl);
+                } catch (IOException e) {
+                    return null;
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int Int() {
+            return Integer.parseInt(next());
+        }
+
+    }
+}
+
