ae64f6bfe3bf8ddc6d5092a6e64afe1faa3d6d5e
daira4000 <daira4000@gmail.com>
2020-07-25 21:02:09 +0900

    [AC] m-solutions2020 m_solutions2020_a

---
+++ atcoder.jp/m-solutions2020/m_solutions2020_a/Main.java
@@ -0,0 +1,70 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        MyScanner in = new MyScanner(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        A solver = new A();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class A {
+        public void solve(int testNumber, MyScanner in, PrintWriter out) {
+            int X = in.Int();
+            if (X < 600) out.println(8);
+            else if (X < 800) out.println(7);
+            else if (X < 1000) out.println(6);
+            else if (X < 1200) out.println(5);
+            else if (X < 1400) out.println(4);
+            else if (X < 1600) out.println(3);
+            else if (X < 1800) out.println(2);
+            else if (X < 2000) out.println(1);
+        }
+
+    }
+
+    static class MyScanner {
+        private BufferedReader in;
+        private StringTokenizer st;
+
+        public MyScanner(InputStream stream) {
+            in = new BufferedReader(new InputStreamReader(stream));
+        }
+
+        public String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    String rl = in.readLine();
+                    if (rl == null) {
+                        return null;
+                    }
+                    st = new StringTokenizer(rl);
+                } catch (IOException e) {
+                    return null;
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int Int() {
+            return Integer.parseInt(next());
+        }
+
+    }
+}
+
