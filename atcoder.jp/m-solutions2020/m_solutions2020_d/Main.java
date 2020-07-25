4e83a4fb7cd18f9cd2151a56effb626275b36134
daira4000 <daira4000@gmail.com>
2020-07-25 22:33:54 +0900

    [AC] m-solutions2020 m_solutions2020_d

---
+++ atcoder.jp/m-solutions2020/m_solutions2020_d/Main.java
@@ -0,0 +1,76 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        MyScanner in = new MyScanner(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        D solver = new D();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class D {
+        public void solve(int testNumber, MyScanner in, PrintWriter out) {
+            int N = in.Int();
+            int[] A = new int[N + 1];
+            for (int i = 0; i < N; i++) {
+                A[i] = in.Int();
+            }
+
+            int idx = 0;
+            long ans = 1000;
+            for (int i = 0; i < N; i++) {
+                if (A[i + 1] < A[i]) {
+                    ans += (ans / A[idx]) * (A[i] - A[idx]);
+                    idx = i + 1;
+                }
+            }
+            out.println(ans);
+        }
+
+    }
+
+    static class MyScanner {
+        private BufferedReader in;
+        private StringTokenizer st;
+
+        public MyScanner(InputStream stream) {
+            in = new BufferedReader(new InputStreamReader(stream));
+        }
+
+        public String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    String rl = in.readLine();
+                    if (rl == null) {
+                        return null;
+                    }
+                    st = new StringTokenizer(rl);
+                } catch (IOException e) {
+                    return null;
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int Int() {
+            return Integer.parseInt(next());
+        }
+
+    }
+}
+
