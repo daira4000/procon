0f7c2ef49911838fe7924a8ecf1850b8c72a34cf
daira4000 <daira4000@gmail.com>
2020-09-26 21:03:37 +0900

    [AC] abl abl_b

---
+++ atcoder.jp/abl/abl_b/Main.java
@@ -0,0 +1,72 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        MyScanner in = new MyScanner(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        B solver = new B();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class B {
+        long A;
+        long B;
+        long C;
+        long D;
+
+        public void solve(int testNumber, MyScanner in, PrintWriter out) {
+            A = in.Long();
+            B = in.Long();
+            C = in.Long();
+            D = in.Long();
+            out.println(A <= D && C <= B ? "Yes" : "No");
+
+        }
+
+    }
+
+    static class MyScanner {
+        private BufferedReader in;
+        private StringTokenizer st;
+
+        public MyScanner(InputStream stream) {
+            in = new BufferedReader(new InputStreamReader(stream));
+        }
+
+        public String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    String rl = in.readLine();
+                    if (rl == null) {
+                        return null;
+                    }
+                    st = new StringTokenizer(rl);
+                } catch (IOException e) {
+                    return null;
+                }
+            }
+            return st.nextToken();
+        }
+
+        public long Long() {
+            return Long.parseLong(next());
+        }
+
+    }
+}
+
