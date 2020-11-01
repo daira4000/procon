0b4ba39797c8a4376459198010244a2917aea2f5
daira4000 <daira4000@gmail.com>
2020-11-01 21:07:06 +0900

    [AC] abc181 abc181_b

---
+++ atcoder.jp/abc181/abc181_b/Main.java
@@ -0,0 +1,75 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        MyScanner in = new MyScanner(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        B solver = new B();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class B {
+        int N;
+        int A;
+        int B;
+
+        public void solve(int testNumber, MyScanner in, PrintWriter out) {
+            N = in.Int();
+            long ans = 0;
+            for (int i = 0; i < N; i++) {
+                A = in.Int();
+                B = in.Int();
+                long d = B - A + 1;
+                long s = A + B;
+                ans += s * (d / 2) + (d % 2 == 1 ? s / 2 : 0);
+            }
+            out.println(ans);
+        }
+
+    }
+
+    static class MyScanner {
+        private BufferedReader in;
+        private StringTokenizer st;
+
+        public MyScanner(InputStream stream) {
+            in = new BufferedReader(new InputStreamReader(stream));
+        }
+
+        public String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    String rl = in.readLine();
+                    if (rl == null) {
+                        return null;
+                    }
+                    st = new StringTokenizer(rl);
+                } catch (IOException e) {
+                    return null;
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int Int() {
+            return Integer.parseInt(next());
+        }
+
+    }
+}
+
